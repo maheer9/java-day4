341284739bbd35507dfeb9ff7dfb582712c0bd32
maheer9 <ammudarlingmaheer@gmail.com>
2024-12-05 15:51:11 +0530

    Add files via upload

---
+++ key.java
@@ -0,0 +1,20 @@
+import java .util.*;
+public class key{
+    public static void main(String args[])
+    {
+        Scanner Sc=new Scanner(System.in);
+        int a=Sc.nextInt();
+        int flag=0;
+        if(a>=18)
+        flag=1;
+        switch(flag)
+        {
+            case 0:
+                System.out.println("not eligible");
+                break;
+            case 1:
+                System.out.println("eligible");
+                break;    
+        }
+        }
+}
