7530f2d18d715fe01b8f13ca173d4f8e89f7c221
maheer9 <ammudarlingmaheer@gmail.com>
2024-12-05 15:56:00 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class print{
+    public static void main(String args[])
+    {
+        Scanner Sc=new Scanner(System.in);
+        int a=Sc.nextInt();
+        for(int i=0; i<=5; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
