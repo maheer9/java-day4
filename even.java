9a3f601b7495b18331d6c0522342c8fba863feb3
maheer9 <ammudarlingmaheer@gmail.com>
2024-12-05 15:49:04 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class even
+{
+    public static void main(String args[])
+    {
+        Scanner Sc=new Scanner(System.in);
+        int a=Sc.nextInt();
+        for(int i=0; i<a; i+=2)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
